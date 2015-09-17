d5cf77ec31e8fd555f7039f4261841c13489c81a
Jordan C. McRae <jcmcrae@prodigy.net>
2015-09-17 01:40:21 -0500

    jQuery and Bootstrap Integrated into web page
    Bootstrap.js moved inside css folder, 1 jQuery function has been added
    and commented out, pre-exisiting function in Hayabusa.js has been
    updated.

--- scripts/Hayabusa.js
+++ scripts/Hayabusa.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
+//	$('#title').hover(function(){
+//		$('#title').animate({border-color: "green"}, 1000);
+//	});
+//The above code is to animate a "click here" type of deal, where it prompts the user to click to play the game.
+	
 	$('#title').click(function(){
-		$('#title').animate({opacity: 0.0, left: "-500px"}, 2000);
+		$('#title').animate({opacity: 0.0, top: "-500px"}, 2000);
+		$('#gameScreen').animate({opacity: 1.0, 
+			bottom: "550px",
+			width: "1000px",
+			height: "500px"}, 2500)
 	});
 });
