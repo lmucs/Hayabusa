b582f42a8cab950d9d14886e1dd1bfe7cafd5a1f
Maurice Leavell <mauriceleavell@gmail.com>
2015-12-12 17:38:52 -0800

    Revert "Revert "Revert "Helped out with Database"""
    This reverts commit b8a82224600f4f92b358178d44a15d824170d3c1.

--- Test2D/Plugins/SQLite3UE4Plugin/Source/SQLite3UE4Plugin/Private/SQLite3UE4Plugin.cpp
+++
@@ -1,5 +0,0 @@
-#include "SQLite3UE4PluginPrivatePCH.h"
-
-DEFINE_LOG_CATEGORY(LogDatabase)
-
-IMPLEMENT_MODULE(FDefaultGameModuleImpl, SQLite3UE4Plugin)
--- Test2D/Source/Test2D/Test2D.Build.cs
+++ Test2D/Source/Test2D/Test2D.Build.cs
@@ -8,7 +8,7 @@ public class Test2D : ModuleRules
 	{
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {"VaRestPlugin"  });
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
