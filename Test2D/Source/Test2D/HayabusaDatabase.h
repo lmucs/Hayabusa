2ba2c2779dc9afb6d6293efe00083424b72a994b
Maurice Leavell <mauriceleavell@gmail.com>
2015-12-12 18:12:15 -0800

    Revert "Revert "Working on Database""
    This reverts commit 26a52a67844c49cb93cf33f2a88c16b83f8f657a.

---
+++ Test2D/Source/Test2D/HayabusaDatabase.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+/**
+ * 
+ */
+class TEST2D_API HayabusaDatabase
+{
+public:
+	HayabusaDatabase();
+	~HayabusaDatabase();
+};
