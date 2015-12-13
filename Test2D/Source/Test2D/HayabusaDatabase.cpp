b8a82224600f4f92b358178d44a15d824170d3c1
Maurice Leavell <mauriceleavell@gmail.com>
2015-12-12 17:38:08 -0800

    Revert "Revert "Helped out with Database""
    This reverts commit 8bb4dba6fce013b319a58e2d05c1301b67c2e5c7.

--- Test2D/Source/Test2D/HayabusaDatabase.cpp
+++ Test2D/Source/Test2D/HayabusaDatabase.cpp
@@ -1,78 +1,42 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Test2D.h"
-#include "HayabusaDatabase.h"
-
-HayabusaDatabase::HayabusaDatabase()
-{
 #include <iostream>
 #include <pqxx/pqxx> 
 
-	using namespace std;
-	using namespace pqxx;
+using namespace std;
+using namespace pqxx;
 
-	int main(int argc, char* argv[])
-	{
-		try{
-			connection C("dbname=testdb user=postgres password=cohondob \
-						       hostaddr=127.0.0.1 port=5432");
-			if (C.is_open()) {
-				cout << "Opened database successfully: " << C.dbname() << endl;
-			}
-			else {
-				cout << "Can't open database" << endl;
-				return 1;
-			}
-			C.disconnect();
-		}
-		catch (const std::exception &e){
-			cerr << e.what() << std::endl;
-			return 1;
-		}
-	}
-}
-
-HayabusaDatabase::~HayabusaDatabase()
+int main(int argc, char* argv[])
 {
-#include <iostream>
-#include <pqxx/pqxx> 
+	char * sql;
 
-	using namespace std;
-	using namespace pqxx;
-
-	int main(int argc, char* argv[])
-	{
-		char * sql;
-
-		try{
-			connection C("dbname=testdb user=postgres password=cohondob \
-						       hostaddr=127.0.0.1 port=5432");
-			if (C.is_open()) {
-				cout << "Opened database successfully: " << C.dbname() << endl;
-			}
-			else {
-				cout << "Can't open database" << endl;
-				return 1;
-			}
-			/* Create SQL statement */
-			sql = "CREATE TABLE SCORES("  \
-				"ID INT PRIMARY KEY     NOT NULL," \
-				"NAME           TEXT    NOT NULL," \
-				"sCORE            INT     NOT NULL," \
-			/* Create a transactional object. */
-			work W(C);
-
-			/* Execute SQL query */
-			W.exec(sql);
-			W.commit();
-			cout << "Table created successfully" << endl;
-			C.disconnect();
+	try{
+		connection C("dbname=testdb user=postgres password=cohondob \
+					       hostaddr=127.0.0.1 port=5432");
+		if (C.is_open()) {
+			cout << "Opened database successfully: " << C.dbname() << endl;
 		}
-		catch (const std::exception &e){
-			cerr << e.what() << std::endl;
+		else {
+			cout << "Can't open database" << endl;
 			return 1;
 		}
+		/* Create SQL statement */
+		sql = "CREATE TABLE COMPANY("  \
+			"ID INT PRIMARY KEY     NOT NULL," \
+			"NAME           TEXT    NOT NULL," \
+			"SCORE            INT     NOT NULL);";
 
-		return 0;
+		/* Create a transactional object. */
+		work W(C);
+
+		/* Execute SQL query */
+		W.exec(sql);
+		W.commit();
+		cout << "Table created successfully" << endl;
+		C.disconnect();
 	}
+	catch (const std::exception &e){
+		cerr << e.what() << std::endl;
+		return 1;
+	}
+
+	return 0;
 }
